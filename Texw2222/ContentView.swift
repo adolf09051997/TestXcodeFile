744760b964301aa124c37a842f44fb704a2cb904
Hung Yu Liao <>
2021-09-22 20:37:06 +0800

    Initial Commit

---
+++ Texw2222/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Texw2222
+//
+//  Created by Hung Yu Liao on 2021/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
